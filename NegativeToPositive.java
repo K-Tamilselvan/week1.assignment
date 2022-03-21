b71f79119b6fdc760b19c9b9ee355cadd02772eb
K-Tamilselvan <102037410+K-Tamilselvan@users.noreply.github.com>
2022-03-21 18:45:29 +0530

    Add files via upload

---
+++ NegativeToPositive.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class NegativeToPositive {
+
+	public static void main(String[] args) {
+	int num=-40;
+	int positivenumber;
+
+	if(num<0) {
+		positivenumber=num*-1;
+		
+		System.out.println(positivenumber);
+		
+		
+	}
+
+	}
+
+}
